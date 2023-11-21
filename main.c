28c1ea7b35ca8d6be7626af648bf35d168abddaf
HoussemBousmaha <ky_bousmaha@esi.dz>
2023-11-21 11:52:05 +0300

    feat: Add bullet firing functionality

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include "SDL2/SDL_rect.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdbool.h>
@@ -10,27 +11,40 @@
 
 typedef struct {
   SDL_Rect pos;
-  bool up, down, left, right;
+  bool up, down, left, right, fire;
 } Player;
 
+typedef struct {
+  SDL_Rect pos;
+  int dx, dy;
+  bool alive;
+} Bullet;
+
 int main(void) {
-  const char *filename = "player.png";
+  const char *player_asset_filename = "player.png";
+  const char *bullet_asset_filename = "bullet.png";
+
   SDL_Init(SDL_INIT_VIDEO);
   SDL_Window *window = SDL_CreateWindow("Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, HEIGHT, WIDTH, SDL_WINDOW_OPENGL);
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+  SDL_Texture *player_texture = IMG_LoadTexture(renderer, player_asset_filename);
+  SDL_Texture *bullet_texture = IMG_LoadTexture(renderer, bullet_asset_filename);
 
-  SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+  SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", player_asset_filename);
 
   bool quit = false;
   Player player = {0};
+  Bullet bullet = {0};
 
   while (!quit) {
     SDL_RenderClear(renderer);
     SDL_SetRenderDrawColor(renderer, 24, 24, 24, 255);
 
-    SDL_QueryTexture(texture, NULL, NULL, &player.pos.w, &player.pos.h);
-    SDL_RenderCopy(renderer, texture, NULL, &player.pos);
+    SDL_QueryTexture(player_texture, NULL, NULL, &player.pos.w, &player.pos.h);
+    SDL_QueryTexture(bullet_texture, NULL, NULL, &bullet.pos.w, &bullet.pos.h);
+    SDL_RenderCopy(renderer, player_texture, NULL, &player.pos);
+    if (bullet.alive)
+      SDL_RenderCopy(renderer, bullet_texture, NULL, &bullet.pos);
 
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -55,6 +69,10 @@ int main(void) {
           if (event.key.keysym.sym == SDLK_RIGHT) {
             player.right = true;
           }
+
+          if (event.key.keysym.sym == SDLK_SPACE) {
+            player.fire = true;
+          }
         }
       }
 
@@ -74,6 +92,10 @@ int main(void) {
 
           if (event.key.keysym.sym == SDLK_RIGHT) {
             player.right = false;
+          }
+
+          if (event.key.keysym.sym == SDLK_SPACE) {
+            player.fire = false;
           }
         }
       }
@@ -102,6 +124,20 @@ int main(void) {
       if (player.pos.x + player.pos.w >= WIDTH)
         player.pos.x -= player.pos.x + player.pos.w - WIDTH;
     }
+
+    if (player.fire) {
+      bullet.pos.x = player.pos.x + player.pos.w / 2 + bullet.pos.w / 2;
+      bullet.pos.y = player.pos.y + player.pos.h / 2 - bullet.pos.h / 2;
+      bullet.dx = 16;
+      bullet.dy = 0;
+      bullet.alive = true;
+    }
+
+    bullet.pos.x += bullet.dx;
+    bullet.pos.y += bullet.dy;
+
+    if (bullet.pos.x > WIDTH)
+      bullet.alive = false;
 
     SDL_RenderPresent(renderer);
 
